c10df5eaaa7493a7d6a6248b43c56740ce5cc039
Artem Prudishew <prudishew@yandex.ru>
2022-12-27 17:52:51 +0300

    Add default test

---
+++ tests/Repositiry/defaultTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Repositiry;
+
+use PHPUnit\Framework\TestCase;
+
+class defaultTest extends TestCase
+{
+    public function testDefault()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Service/defaultTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class defaultTest extends TestCase
+{
+    public function testDefault()
+    {
+        $this->assertTrue(true);
+    }
+}
